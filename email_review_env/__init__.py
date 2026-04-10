89ee804b001a14b3bc21c466499f53ef7475cc2a
Aneesh1829 <aneeshreddy005@gmail.com>
2026-04-10 07:47:20 +0530

    Fix circular import in package init

--- email_review_env/__init__.py
+++ email_review_env/__init__.py
@@ -1,18 +1,1 @@
-from .env import EmailReviewEnvironment
-from .graders import GRADERS, grade_task, grade_task_1, grade_task_2, grade_task_3
-from .models import EmailAction, EmailObservation
-from .tasks import TASKS, TASK_MAP, TASK_REGISTRY
-
-__all__ = [
-    "EmailAction",
-    "EmailObservation",
-    "EmailReviewEnvironment",
-    "TASKS",
-    "TASK_MAP",
-    "TASK_REGISTRY",
-    "GRADERS",
-    "grade_task",
-    "grade_task_1",
-    "grade_task_2",
-    "grade_task_3",
-]
+__all__ = []
